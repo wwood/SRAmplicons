1bc6806a7179e9bbac7e0ca23257cc048baeb6ce
Ben J Woodcroft <donttrustben@gmail.com>
2012-08-06 13:56:41 +1000

    a new rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sramplicons::Application.config.secret_token = '186655d8d5797cb01f3acdc719ba830cfc1c8890afd699ee032c638f209f820bc49b9b0bf6320db321ef05bc10289c85badd59b598db29ffea349124c03f78e9'
