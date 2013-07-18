e36881a48f870f824418d1de42dbb85f4914ba1a
Ben J. Woodcroft <donttrustben near gmail.com>
2013-07-18 20:30:48 +1000

    Check the connection to the SRAdb at boot

--- config/environment.rb
+++ config/environment.rb
@@ -13,4 +13,8 @@ Sramplicons::Application.initialize!
 require 'bio-sra'
 sradb_path = File.join(File.dirname(__FILE__),'..','db','SRAmetadb.sqlite3')
 $stderr.puts "Connecting to SRAdb #{sradb_path}"
-puts Bio::SRA::Connection.connect(sradb_path)
+connection = Bio::SRA::Connection.connect(sradb_path)
+$stderr.puts "May or may not be connected, lets test.."
+if Bio::SRA::Tables::SRA.first
+  $stderr.puts "Appear to be connected to the SRAdb, good."
+end
